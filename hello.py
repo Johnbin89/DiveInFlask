06820d8075f949dc769ecdb7caf4bb2783adaac7
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2019-09-06 19:00:23 +0300

    3b

--- hello.py
+++ hello.py
@@ -1,13 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    manager.run()
