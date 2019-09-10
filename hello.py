a10867dd128d49392578bf860b629b0bfd6aefdf
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2019-09-10 13:44:29 +0300

    3emyflask

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,7 +20,7 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
