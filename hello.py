228bfccd030257c3002fef602fb7adcc9444def5
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2019-09-11 15:06:37 +0300

    4amyflask

--- hello.py
+++ hello.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_script import Manager
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 's12567dg'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
@@ -20,9 +27,14 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.date = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
