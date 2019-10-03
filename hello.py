8a31de00eba12d768aee6e3e93c97e07ecfb05c4
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2019-10-03 21:27:13 +0300

    5cmyflask

--- hello.py
+++ hello.py
@@ -3,11 +3,14 @@ from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+
+def make_shell_context():                                                  #integrade db with python shell
+    return dict(app=app, db=db, User=User, Role=Role)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,6 +28,7 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 manager = Manager(app)
+manager.add_command("shell", Shell(make_context=make_shell_context))   #integrade db with python shell
 
 db = SQLAlchemy(app)
 
@@ -59,13 +63,17 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known = session.get('known', False), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
