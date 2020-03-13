9c6f4d632edccea542ff83f439ac4e3a900817a1
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2020-03-13 17:59:41 +0200

    Create Login

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import func, DateTime
+from flask_login import UserMixin
+from . import login_manager
 
 '''
 class Role(db.Model):
@@ -19,6 +23,29 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' %self.username
 '''
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password_hash = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Divespot(db.Model):
     __tablename__ = 'divespots'
     id = db.Column(db.Integer, primary_key=True)
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-#from app.models import User, Role
+from app.models import *
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -10,7 +10,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():                                                  #integrade db with python shell
     #return dict(app=app, db=db, User=User, Role=Role)
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=User)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))   #integrade db with python shell
 manager.add_command('db', MigrateCommand)
